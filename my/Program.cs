b0c39eb45867aca9a24c79dd7299c7adeff79ad5
Rectale <vyacheslav-shkoda@mail.ru>
2022-12-03 01:33:22 +0300

    Настоящая работа с массивами, Изменена общая форма документа

--- my/Program.cs
+++ my/Program.cs
@@ -1,43 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using System.Globalization;
+﻿using System;
+using System.Linq;
 
-namespace Lessons
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            System.Console.Write("Сколько у вас денег(Руб, Евр, Юан) ('сумма' 'валюта'): ");
-            string user_val = Console.ReadLine();
-            string[] user_val_m = user_val.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+// int[] meas = { 1,4,77,23,54,87,87,10,55,32,55,77,1,4 };
+// System.Console.WriteLine(meas.Average());
+// System.Console.WriteLine(meas.Min());
+// System.Console.WriteLine(meas.Where(i => i%2 == 0).Sum());
+// System.Console.WriteLine(meas.Where(i => i%2 == 1).Min());
 
-            double rub = 0.016, eur = 1.05, yan = 0.14;
+// System.Console.WriteLine();
+// int[] res_mnojestvo = meas.Distinct().ToArray();
+// for (int i = 0; i < res_mnojestvo.Length; i++) System.Console.Write(res_mnojestvo[i]+" ");
 
-            double user_money;
-            bool res_money_convert = double.TryParse(user_val_m[0],out user_money);
-            bool res_val_convert = (user_val_m[1] == "Руб"|user_val_m[1] == "Евр"|user_val_m[1] == "Юан");
+// System.Console.WriteLine();
+// int[] sort_res = meas.OrderBy(i => i).ToArray(); //OrderByDecending - в обратном порядке(сортировка)
+// for (int i = 0; i < sort_res.Length; i++) System.Console.Write(sort_res[i]+" ");
 
-            if (res_money_convert & res_val_convert)
-            {
-                switch (user_val_m[1])
-                {
-                    case "Руб":
-                        System.Console.WriteLine("У тебя: " + (user_money * rub) + " $");
-                        break;
-                    case "Евр":
-                        System.Console.WriteLine("У тебя: " + (user_money * eur) + " $");
-                        break;
-                    case "Юан":
-                        System.Console.WriteLine("У тебя: " + (user_money * yan) + " $");
-                        break;
-                }
-            }
-            else
-            {
-                System.Console.WriteLine("Ошибка ввода");
-            }
-        }
-    }
-}
+// System.Console.WriteLine();
+
+// Array.Sort(meas);
+// Array.Find(meas, i => i > 50); //Find - превый встречный, FindLast - первый с конца
+// Array.FindAll(meas, i => i > 50); // Создаёт массив с ними
+// System.Console.WriteLine(Array.FindIndex(meas,i => i == 55)); //Ищет индекс, если нет, то -1
+// Array.Reverse(meas); //Разворачивает массив
 
