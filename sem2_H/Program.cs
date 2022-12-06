b33240797f30fb4e8264d7ddacf34f19920d2048
Rectale <vyacheslav-shkoda@mail.ru>
2022-12-07 00:26:03 +0300

    Добавлена 10 задача

---
+++ sem2_H/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Задача 10");
+while(true)
+{
+    Random random = new Random();
+    int x = random.Next(100,1000);
+    System.Console.WriteLine(x);
+    System.Console.WriteLine((x % 100)/10);
+
+    System.Console.WriteLine("Продолжим? (след. задание - q) ");
+    string msg = Console.ReadLine();
+    if (msg == "q")break;
+}
+
+System.Console.WriteLine();
+
